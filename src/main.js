55a6d5a9620e61856b8948609bec32d12dc75f6c
Santino Stimoli <107507086+SantinoStimoli@users.noreply.github.com>
2023-11-08 11:41:10 -0300

    squema

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
+// APP
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// STYLE
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import './style.css'
+import 'primeicons/primeicons.css'
+
+// COMPONENTS
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Dialog from 'primevue/dialog'
+
+// CREATE
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('InputNumber', InputNumber)
+app.component('Dialog', Dialog)
+app.mount('#app')
